f933106037c8d475a3ba0e8f6ecd00f9cb266e48
kyonkyonkyonkyon <k.kusano.8.19@gmail.com>
2020-05-17 14:43:25 +0900

    Implementation_completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "notes#index"
   resources :users, only: [:edit, :update]
-  resources :notes, only: [:index, :new, :create, :show]
+  resources :notes, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 end
