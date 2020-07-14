7455cd88c10de9e0cad60edaeb4ca3ff63ebff28
kyonkyonkyonkyon <k.kusano.8.19@gmail.com>
2020-07-14 23:26:53 +0900

    add_validation_message

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,15 @@ Bundler.require(*Rails.groups)
 
 module VoiceNote
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    # デフォルトのlocaleを日本語(:ja)にする
+    config.i18n.default_locale = :ja
+    #以下の記述を追記する(設定必須)
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
+    # config.load_defaults 5.2
+
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
@@ -17,4 +26,5 @@ module VoiceNote
   end
 end
 
+# config.i18n.default_locale = :ja
 # config.assets.initialize_on_precompile = false
